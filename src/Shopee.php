404ca695fd9a249320384f8d3943594af7881b7f
erayashi <eraya.ashi@gmail.com>
2018-10-29 17:12:27 +0700

    initial

---
+++ src/Shopee.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by eaz.
+ * Date: 29/10/18
+ * Time: 17:04
+ * Github: https://github.com/erry-az
+ */
+
+namespace ErryAz\ShopeeWrap;
+
+
+class Shopee
+{
+
+}
---
+++ src/models/Items.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by eaz.
+ * Date: 29/10/18
+ * Time: 17:05
+ * Github: https://github.com/erry-az
+ */
+
+namespace ErryAz\ShopeeWrap\models;
+
+
+class Items
+{
+
+}
